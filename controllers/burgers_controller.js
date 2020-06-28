04a8f7b8bd989727d9adeafc4a1d403ecd90fd88
ICVRXS <calebhay@rocketmail.com>
2020-06-28 16:54:55 -0700

    Started framework for router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const burgers = require("../models/burger");
+
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    
+});
+
+module.exports = router;
