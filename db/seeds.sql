7efc783137d5fbf6de6e560f97097e34bd3d76c8
ICVRXS <calebhay@rocketmail.com>
2020-06-28 16:31:56 -0700

    Folder/file structure created, database created

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Western Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Impossible Burger", false);
