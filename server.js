177cddd022e72fc07e5c62c086cbb9d68e9601d7
unknown <calebhay@rocketmail.com>
2020-09-23 18:06:53 -0700

    Added JAWSDB to connection.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const burgerConrtoller = require("./controllers/burgers_controller");
+const burgerController = require("./controllers/burgers_controller");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7070;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -13,12 +13,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(burgerConrtoller);
+app.use(burgerController);
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 app.listen(PORT, (err) => {
-    if(err) throw err;
     console.log(`Listening on port ${PORT}`);
 });
